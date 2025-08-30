b489ca0f0510ef3d6f4370c5ae07c871cfb6645a
hyoneekim <suhyunkim@Suhyunui-MacBookAir.local>
2025-08-30 09:22:34 +0300

    Complete FE Pair Programming 1 Part 1

--- Dev/week2/fe-pp1-part1/src/MainContent.jsx
+++ Dev/week2/fe-pp1-part1/src/MainContent.jsx
@@ -1,4 +1,6 @@
 import IdCard from "./IdCard";
+import "./index.css"
+import Random from "./Random";
 
 function MainContent(){
     return (
@@ -20,7 +22,10 @@ function MainContent(){
   birth={new Date("1988-05-11")}
   picture="https://randomuser.me/api/portraits/women/44.jpg"
 />
+<Random min={1} max={6}/>
+<Random min={1} max = {108}/>
     </div>
+
     )
 }
 
